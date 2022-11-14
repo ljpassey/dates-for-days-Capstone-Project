dd97785f5a03c9287f50f14c622e167bece471e3
Luke Passey <ljpassey@gmail.com>
2022-11-14 14:46:33 -0700

    This commit should work for deployment

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+//Imports
 require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
@@ -7,11 +8,21 @@ const app = express()
 
 const { getDateDetails, addDate } = require('./controller')
 
-app.get('/', function(req,res) {
-    res.sendFile(path.join(__dirname, ''))
-})
+
+//Middleware
 app.use(express.json())
 app.use(cors())
+
+//Endpoints connecting HTML, CSS, and JS
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, '../client/index.html')))
+app.get('/indexCSS', (req, res) => res.sendFile(path.join(__dirname, '../client/index.html')))
+app.get('/indexJS', (req, res) => res.sendFile(path.join(__dirname, '../client/index.html')))
+app.get('/dgHTML', (req, res) => res.sendFile(path.join(__dirname, '../client/index.html')))
+app.get('/dgCSS', (req, res) => res.sendFile(path.join(__dirname, '../client/index.html')))
+app.get('/dgJS', (req, res) => res.sendFile(path.join(__dirname, '../client/index.html')))
+app.get('/profHTML', (req, res) => res.sendFile(path.join(__dirname, '../client/index.html')))
+app.get('/profCSS', (req, res) => res.sendFile(path.join(__dirname, '../client/index.html')))
+app.get('/profJS', (req, res) => res.sendFile(path.join(__dirname, '../client/index.html')))
 
 app.get('/date', getDateDetails)
 app.post('/date', addDate)
