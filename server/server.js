5cdf99ade566c388c4668b00782539d9093a415a
Luke Passey <ljpassey@gmail.com>
2022-11-14 13:55:19 -0700

    This commit includes updated capstone project that should work for deployment.

--- server/server.js
+++ server/server.js
@@ -1,15 +1,21 @@
 require('dotenv').config()
 const express = require('express')
-const app = express()
 const cors = require('cors')
-const {SERVER_PORT} = process.env
+const path = require('path')
+
+const app = express()
 
 const { getDateDetails, addDate } = require('./controller')
 
+app.get('/', function(req,res) {
+    res.sendFile(path.join(__dirname, ''))
+})
 app.use(express.json())
 app.use(cors())
 
 app.get('/date', getDateDetails)
 app.post('/date', addDate)
+
+const {SERVER_PORT} = process.env
 
 app.listen(SERVER_PORT, () => console.log(`up on ${SERVER_PORT}`))
